febae981ac9050f53b60cb66ebc11f000646acb6
572233093@qq.com <572233093@qq.com>
2020-09-08 08:37:42 +0800

    第一次提交哦啊

---
+++ src/com/Mytest.java
@@ -0,0 +1,17 @@
+package com;
+
+public class Mytest {
+
+	public static void main(String[] args) {
+		System.out.println("哈哈哈哈哈");
+		Mytest.test();
+		// TODO Auto-generated method stub
+
+	}
+
+	private static void test() {
+		System.out.println("1111");
+
+	}
+
+}
